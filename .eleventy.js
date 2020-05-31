d1c53cb5e043e829f51964d99d65a65b6a4a0e18
Sergio Martín <sergio@sergiomartin.dev>
2020-05-31 02:34:18 +0200

    Add syntaxHighlight plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function(config) {
   config.addPassthroughCopy("src/assets");
+  config.addPlugin(syntaxHighlight);
   return {
     dir: {
       input: "src",
