643bb6f2ccf64fd75c5435b6d610b44d9e7153bc
Sergio Martín <sergio@sergiomartin.dev>
2020-06-10 23:53:31 +0200

    Change resizer script output filenames

--- img/resizer.js
+++ img/resizer.js
@@ -37,9 +37,12 @@ async function resize(originalFileName, width, outputPath, newFileName) {
 
 const outputPath = "../src/assets/images/";
 
-resize('avatar.jpg', 180, outputPath, 'avatar-1x.jpg');
-resize('avatar.jpg', 360, outputPath, 'avatar-2x.jpg');
-resize('avatar.jpg', 540, outputPath, 'avatar-3x.jpg');
-resize('avatar.jpg', 720, outputPath, 'avatar-4x.jpg');
+resize('avatar.jpg', 180, outputPath, 'avatar-180w.jpg');
+resize('avatar.jpg', 360, outputPath, 'avatar-360w.jpg');
+resize('avatar.jpg', 540, outputPath, 'avatar-540w.jpg');
+resize('avatar.jpg', 720, outputPath, 'avatar-720w.jpg');
 
-// resize('photoko.jpg', 492, outputPath, 'photoko-1x.jpg');
+resize('photoko.jpg',  320, outputPath, 'photoko-320w.jpg');
+resize('photoko.jpg',  640, outputPath, 'photoko-640w.jpg');
+resize('photoko.jpg',  960, outputPath, 'photoko-960w.jpg');
+resize('photoko.jpg', 1280, outputPath, 'photoko-1280w.jpg');
